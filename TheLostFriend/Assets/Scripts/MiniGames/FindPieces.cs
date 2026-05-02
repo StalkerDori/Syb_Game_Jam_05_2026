27173df24debaf95d1a78dc5a88299fc077216a2
Nikita <skovorodkinns2004@gmail.com>
2026-05-02 23:02:07 +0700

    еще немного

--- TheLostFriend/Assets/Scripts/MiniGames/FindPieces.cs
+++ TheLostFriend/Assets/Scripts/MiniGames/FindPieces.cs
@@ -1,7 +1,9 @@
+using TMPro;
 using UnityEngine;
 
 public class FindPieces : MonoBehaviour
 {
+    public TextMeshProUGUI countPieces;
     public GameObject[] pieces;
     public static float countFindPieces;
     public static bool eleven;
@@ -16,5 +18,7 @@ public class FindPieces : MonoBehaviour
     {
         if (countFindPieces == 11)
             eleven = true;
+
+        countPieces.text = "Кусочков собрано: " + countFindPieces + "/12";
     }
 }
